764ef17d7b4c24f0ffdf6c5f91302953fac7fec8
tarunajjarapu <tarunajj@gmail.com>
2023-07-15 17:28:41 -0500

    Setup frame using Tkinter

--- web_scraper.py
+++ web_scraper.py
@@ -16,6 +16,19 @@ class Find:
 
 def main():
     url = 'https://www.pro-football-reference.com/years/2022/fantasy.htm'
+    curr = Find()
+    curr.web_scrape(url)
+    origin = Tk()
+    # Setting up the frame of the program
+    origin.title("NFL Fantasy Assistant")
+    origin.resizable(False, False)
+    lbl_frame = ttk.Frame(origin, padding="75 15 15 15")
+    origin.geometry("690x770")
+    lbl_frame.grid(row=13, column=2)
+    pts = Label(lbl_frame, font='Courier 20', text='Points')
+    pts.grid(row=0, column=1, padx=2, pady=2, sticky='W')
+    control_label = Label(lbl_frame, font='Courier 40 bold', text='QB')
+    control_label.grid(row=0, column=1, padx=215, pady=2, sticky='W')
 
 if __name__ == '__main__':
     main()
