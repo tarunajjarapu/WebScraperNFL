0d31bc9f43d99d826547d9875cc579ed05c79af9
tarunajjarapu <tarunajj@gmail.com>
2023-07-15 17:27:19 -0500

    Setup program for web scraping

---
+++ web_scraper.py
@@ -0,0 +1,21 @@
+# Name: Tarun Ajjarapu
+
+import requests
+from bs4 import BeautifulSoup
+from tkinter import *
+from tkinter import ttk
+
+
+class Find:
+
+    def __init__(self):
+        """
+        This initializes the lists of players for all positions
+        """
+
+
+def main():
+    url = 'https://www.pro-football-reference.com/years/2022/fantasy.htm'
+
+if __name__ == '__main__':
+    main()
